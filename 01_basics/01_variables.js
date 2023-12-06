86e93ba317ee6f42ef674debcc2da3ea069a0a15
mohmadsahib <152958476+mohmadsahib@users.noreply.github.com>
2023-12-06 15:42:23 +0000

    discussion on variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "mohmad@1234";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+// accountId = 11113; not allowed
+accountEmail = "mohmad@49899475"
+accountPassword = "34979734"
+accountCity = "Bangular"  
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
